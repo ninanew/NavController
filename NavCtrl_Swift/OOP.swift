eb6a793e08d3444a5f25c92c6e7aaa58e0e5417b
Kristina Neuwirth <kristina.neuwirth11@gmail.com>
2018-09-19 18:00:29 -0400

    oop

---
+++ NavCtrl_Swift/OOP.swift
@@ -0,0 +1,49 @@
+//
+//  OOP.swift
+//  NavCtrl_Swift
+//
+//  Created by Kristina Neuwirth on 9/19/18.
+//  Copyright © 2018 turntotech. All rights reserved.
+//
+
+import Foundation
+
+
+class Company {
+    
+    var products = ["Apple iPad Pro", "Apple iPod Touch", "Apple iPhone", "Samsung Galaxy S9", "Samsung Galaxy Note", "Samsung Galaxy Tab", "Amazon Echo Dot", "Amazon Echo", "Amazon Fire HD", "Windows Phone 8", "Microsoft Nokia Lumia", "Windows Surface"]
+    
+    init() {
+        
+        self.products = ["products"]
+
+    }
+    
+
+}
+
+    var appleMobileDevices = Company()
+    var samsungMobileDevices = Company()
+    var amazonMobieDevices = Company()
+    var microsoftMobileDevices = Company()
+
+class Products: Company {
+    
+
+    
+    }
+
+    var appleIphone = Products()
+    var appleIpadPro = Products()
+    var appleIpodTouch = Products()
+    var samsungGalaxyS = Products()
+    var samsungGalaxyTab = Products()
+    var samsungGalaxyNote = Products()
+    var amazonEchoDot = Products()
+    var amazonEcho = Products()
+    var amazonFireHD = Products()
+    var microsoftNokiaLumia = Products()
+    var windowsPhone = Products()
+    var windowsSurface = Products()
+
+
